6b2cf9ef7b4287bb5bed829d26f156676a71fba3
stratosys <ashish@logbullit.com>
2016-05-23 06:54:40 +0000

    Subsite initial commit

--- sites/sites.php
+++ sites/sites.php
@@ -56,3 +56,4 @@
 
 $sites['sitevs0348.evolverinc.tech'] = 'all';
 $sites['subsitevs0010.evolverinc.tech'] = 'subsitevs0010.evolverinc.tech';
+$sites['subsitevs0012.evolverinc.tech'] = 'subsitevs0012.evolverinc.tech';
